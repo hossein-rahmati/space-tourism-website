5235fd40f1043f848acb447c359f1f9463330355
hossein Rahmati <rmthossein@gmail.com>
2022-07-10 01:58:07 +0430

    initiated the project

---
+++ src/JS/home.js
@@ -0,0 +1,16 @@
+const navHamburger = document.querySelector(".nav__hamburger");
+const backDrop = document.querySelector(".nav__backdrop");
+const navItems = document.querySelector(".nav__items");
+
+
+navHamburger.addEventListener("click", () => {
+    navHamburger.style.display = "none";
+    backDrop.style.display = "block";
+    navItems.style.display = "flex";
+})
+
+backDrop.addEventListener("click", () => {
+    navHamburger.style.display = "block";
+    backDrop.style.display = "none";
+    navItems.style.display = "none";
+})
