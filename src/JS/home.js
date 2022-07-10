80d590c78752ed500085059951c5b7fdde1ca1e6
hossein Rahmati <rmthossein@gmail.com>
2022-07-10 15:32:42 +0430

    'Destinatio' & 'Crew' pages developed

--- src/JS/home.js
+++ src/JS/home.js
@@ -2,15 +2,14 @@ const navHamburger = document.querySelector(".nav__hamburger");
 const backDrop = document.querySelector(".nav__backdrop");
 const navItems = document.querySelector(".nav__items");
 
-
 navHamburger.addEventListener("click", () => {
-    navHamburger.style.display = "none";
-    backDrop.style.display = "block";
-    navItems.style.display = "flex";
-})
+  navHamburger.style.display = "none";
+  backDrop.style.display = "block";
+  navItems.style.display = "flex";
+});
 
 backDrop.addEventListener("click", () => {
-    navHamburger.style.display = "block";
-    backDrop.style.display = "none";
-    navItems.style.display = "none";
-})
+  navHamburger.style.display = "block";
+  backDrop.style.display = "none";
+  navItems.style.display = "none";
+});
